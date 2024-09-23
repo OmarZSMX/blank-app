b4f7f11ce5390c57d6e83be571959ef85a485000
OmarZSMX <mxomarcc@gmail.com>
2024-09-23 12:31:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-pip install streamlit pandas
 import streamlit as st
 import pandas as pd
 import math
